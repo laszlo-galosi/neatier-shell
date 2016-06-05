74353ea33580c07ac5649f57ecbe8e1a1b139809
laszlo_galosi <laszlo.galosi78@gmail.com>
2016-06-05 22:30:16 +0200

    Initial application shell commit.

---
+++ app/src/main/java/com/neatier/shell/activities/SplashActivity.java
@@ -0,0 +1,34 @@
+/*
+ *  Copyright (C) 2016 Delight Solutions Ltd., All Rights Reserved
+ *  Unauthorized copying of this file, via any medium is strictly prohibited.
+ *  Proprietary and confidential.
+ *
+ *  All information contained herein is, and remains the property of Delight Solutions Kft.
+ *  The intellectual and technical concepts contained herein are proprietary to Delight Solutions
+  *  Kft.
+ *   and may be covered by U.S. and Foreign Patents, pending patents, and are protected
+ *  by trade secret or copyright law. Dissemination of this information or reproduction of
+ *  this material is strictly forbidden unless prior written permission is obtained from
+ *   Delight Solutions Kft.
+ */
+
+package com.neatier.shell.activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by László Gálosi on 06/04/16
+ */
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
